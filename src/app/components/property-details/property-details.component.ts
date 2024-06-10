b9c7757631d86d92077305d268a389209fa2161f
Edson Ascendino <edson.ascendinojr@gmail.com>
2024-06-10 20:13:38 -0300

    feat: Filter changes, adjusting property-details component.

--- src/app/components/property-details/property-details.component.ts
+++ src/app/components/property-details/property-details.component.ts
@@ -1,23 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { Property } from '../../models/property';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription, tap, timer } from 'rxjs';
+import { Property } from '../../models/property';
+
 @Component({
   selector: 'app-property-details',
   templateUrl: './property-details.component.html',
   styleUrls: ['./property-details.component.scss']
 })
-export class PropertyDetailsComponent implements OnInit {
+export class PropertyDetailsComponent implements OnInit, OnDestroy {
   public property!: Property;
+  public contactForm!: FormGroup;
+  public isFormSubmited: boolean = false;
+  public formError: string = 'Please correct the errors in the form.';
+  public formSuccess: string = 'Contact Informations Saved!';
 
-  constructor(private _router: Router) { }
+  private _subscription = new Subscription();
+
+  constructor(
+    private _fb: FormBuilder,
+    private _router: Router
+  ) {
+    this._buildForm();
+  }
 
   ngOnInit(): void {
     this.property = history.state.property;
   }
 
-  public goToPropertyList() {
+  public goToPropertyList(): void {
     this._router.navigate(['/listings']);
   }
 
+  public onSubmit(): void {
+    if (this.contactForm.valid) {
+      this.isFormSubmited = true;
+
+      const resetFormAfterDelay$ = timer(1500).pipe(
+        tap(() => {
+          this.isFormSubmited = false;
+          this.contactForm.reset();
+        })
+      );
+
+      this._subscription.add(
+        resetFormAfterDelay$.subscribe()
+      );
+    }
+  }
+
+
+  private _buildForm(): void {
+    this.contactForm = this._fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      comments: ['', Validators.required]
+    })
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
 
 }
--- src/app/components/property-details/property-details.module.ts
+++ src/app/components/property-details/property-details.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { PropertyDetailsComponent } from './property-details.component';
 import { PropertyDetailsRoutingModule } from './property-details-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +15,11 @@ import { PropertyDetailsRoutingModule } from './property-details-routing.module'
   ],
   imports: [
     CommonModule,
-    PropertyDetailsRoutingModule
+    PropertyDetailsRoutingModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
   ],
   exports: []
 })
